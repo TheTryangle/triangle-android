84520d4730b09be7de33cbdbcb5ce3450511f133
DanielvanBavel <daniel.ajax.1996@rb-media.nl>
2017-06-09 13:13:44 +0200

    code cleanup before working

--- TriangleApp/app/src/main/java/triangle/triangleapp/CameraHelper.java
+++ TriangleApp/app/src/main/java/triangle/triangleapp/CameraHelper.java
@@ -7,13 +7,14 @@ import android.hardware.Camera;
  */
 
 public class CameraHelper {
-  public static Camera getCameraInstance() {
-    Camera c = null;
-    try {
-      c = Camera.open(); // attempt to get a Camera instance
-    } catch (Exception e) {
-      // Camera is not available (in use or does not exist)
+    public static Camera getCameraInstance() {
+
+        Camera c = null;
+        try {
+            c = Camera.open(); // attempt to get a Camera instance
+        } catch (Exception e) {
+            // Camera is not available (in use or does not exist)
+        }
+        return c; // returns null if camera is unavailable
     }
-    return c; // returns null if camera is unavailable
-  }
 }
