9bf7198fc2e89784b9a0da394a1764e132a3d244
Koen de Bont <koen-debont@hotmail.com>
2017-06-15 12:49:30 +0200

    Code Conventies

--- TriangleApp/app/src/main/java/triangle/triangleapp/activities/MainActivity.java
+++ TriangleApp/app/src/main/java/triangle/triangleapp/activities/MainActivity.java
@@ -1,7 +1,6 @@
 package triangle.triangleapp.activities;
 
 import android.Manifest;
-import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
@@ -9,21 +8,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-
-import com.karumi.dexter.Dexter;
-import com.karumi.dexter.MultiplePermissionsReport;
-import com.karumi.dexter.PermissionToken;
-import com.karumi.dexter.listener.DexterError;
-import com.karumi.dexter.listener.PermissionDeniedResponse;
-import com.karumi.dexter.listener.PermissionGrantedResponse;
-import com.karumi.dexter.listener.PermissionRequest;
-import com.karumi.dexter.listener.PermissionRequestErrorListener;
-import com.karumi.dexter.listener.multi.DialogOnAnyDeniedMultiplePermissionsListener;
-import com.karumi.dexter.listener.multi.MultiplePermissionsListener;
-import com.karumi.dexter.listener.multi.SnackbarOnAnyDeniedMultiplePermissionsListener;
-
 import java.util.List;
-
 import pub.devrel.easypermissions.AfterPermissionGranted;
 import pub.devrel.easypermissions.AppSettingsDialog;
 import pub.devrel.easypermissions.EasyPermissions;
@@ -33,7 +18,6 @@ public class MainActivity extends AppCompatActivity implements EasyPermissions.P
 
     private static final String TAG = "MainActivity";
     private static final int PERMISSION_REQUEST_CODE = 123;
-    private static final int GRANT_RESULT_AMOUNT = 3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -53,7 +37,7 @@ public class MainActivity extends AppCompatActivity implements EasyPermissions.P
     }
 
     /**
-     * Continues the initialization flow.
+     * Check if app has all the required permissions
      */
     @AfterPermissionGranted(PERMISSION_REQUEST_CODE)
     private void methodRequiresPermissions() {
@@ -63,11 +47,15 @@ public class MainActivity extends AppCompatActivity implements EasyPermissions.P
                             Manifest.permission.READ_EXTERNAL_STORAGE};
 
         if (!EasyPermissions.hasPermissions(this, perms)) {
+
             // Do not have permissions, request them now
             EasyPermissions.requestPermissions(this, "The asked permissions are required for this app to work.", PERMISSION_REQUEST_CODE, perms);
         }
     }
 
+    /**
+     * Requests the required permissions for this app
+     */
     @Override
     public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
@@ -86,13 +74,13 @@ public class MainActivity extends AppCompatActivity implements EasyPermissions.P
         Log.d(TAG, "onPermissionsDenied:" + requestCode + ":" + perms.size());
 
         // This will display a dialog directing them to enable the permission in app settings.
-            new AppSettingsDialog.Builder(this)
-                .setNegativeButton("Close App", new DialogInterface.OnClickListener() {
-                    public void onClick(DialogInterface dialog, int id) {
-                        MainActivity.this.finish();
-                    }
-                })
-                .build()
-                .show();
+        new AppSettingsDialog.Builder(this)
+            .setNegativeButton("Close App", new DialogInterface.OnClickListener() {
+                public void onClick(DialogInterface dialog, int id) {
+                    MainActivity.this.finish();
+                }
+            })
+            .build()
+            .show();
     }
 }
