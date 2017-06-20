856886e5d0d98384eb485cf56586213c2a37dd05
DaanTacoSchool <djmessen@avans.nl>
2017-06-20 10:40:14 +0200

    alle settings in activity, nog niet geimplementeerd, eerst dev in deze. 10:40 /20-6-40

--- TriangleApp/app/src/main/java/triangle/triangleapp/presentation/impl/ShowSettingsActivity.java
+++ TriangleApp/app/src/main/java/triangle/triangleapp/presentation/impl/ShowSettingsActivity.java
@@ -29,10 +29,19 @@ public class ShowSettingsActivity extends Activity {
         builder.append("n" + sharedPrefs.getString("username", "NULL"));
 
         // Category 2, Security
-        builder.append("n" + sharedPrefs.getString("hash_algorithm", "NULL"));
+        builder.append("n" + sharedPrefs.getString("key_algorithm", "NULL"));
+        builder.append("n" + sharedPrefs.getString("sign_algorithm", "NULL"));
+        builder.append("n" + sharedPrefs.getInt("key_size",1024));
+        builder.append("n" + sharedPrefs.getString("key_alias", "NULL"));
+        builder.append("n" + sharedPrefs.getString("key_store", "NULL"));
 
         //Category 3, Connection
         builder.append("n" + sharedPrefs.getString("stream_destination_address", "NULL"));
+
+        // Category 4, Device
+        builder.append("n" + sharedPrefs.getString("file_helper_save_location", "NULL"));
+        builder.append("n" + sharedPrefs.getString("max_record_duration","NULL"));
+        builder.append("n" + sharedPrefs.getString("display_orientation","NULL"));
 
         TextView settingsTextView = (TextView) findViewById(R.id.settings_text_view);
         settingsTextView.setText(builder.toString());
