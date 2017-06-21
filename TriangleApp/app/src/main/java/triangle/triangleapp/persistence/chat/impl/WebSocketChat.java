352caad48777833cad0ee29541bb000863382165
Marco <marcohavermans+github@hotmail.nl>
2017-06-21 15:22:35 +0200

    Added missing serializer for dates.

--- TriangleApp/app/src/main/java/triangle/triangleapp/persistence/chat/impl/WebSocketChat.java
+++ TriangleApp/app/src/main/java/triangle/triangleapp/persistence/chat/impl/WebSocketChat.java
@@ -8,6 +8,9 @@ import com.google.gson.JsonDeserializationContext;
 import com.google.gson.JsonDeserializer;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonParseException;
+import com.google.gson.JsonPrimitive;
+import com.google.gson.JsonSerializationContext;
+import com.google.gson.JsonSerializer;
 import com.koushikdutta.async.http.AsyncHttpClient;
 import com.koushikdutta.async.http.WebSocket;
 
@@ -40,6 +43,13 @@ public class WebSocketChat implements ChatAdapter {
         builder.registerTypeAdapter(Date.class, new JsonDeserializer<Date>() {
             public Date deserialize(JsonElement json, Type typeOfT, JsonDeserializationContext context) throws JsonParseException {
                 return new Date(json.getAsJsonPrimitive().getAsLong());
+            }
+        });
+
+        builder.registerTypeAdapter(Date.class, new JsonSerializer<Date>() {
+            @Override
+            public JsonElement serialize(Date date, Type type, JsonSerializationContext jsonSerializationContext) {
+                return new JsonPrimitive(date.getTime());
             }
         });
 
